e9af85d7f371d329b5ad4c54910ee7f231296da2
Aafthab Ashraff <aafthab.ashraff@gmail.com>
2022-07-24 22:22:03 +0100

    Layout components where created for the pages

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,11 @@
 import Head from 'next/head';
 import Image from 'next/image';
 import styles from '../styles/Home.module.css';
-import Navbar from '../components/Navbar';
-import Footer from '../components/Footer';
 import Link from 'next/link';
 
 export default function Home() {
   return (
     <div>
-      <Navbar />
       <h1>Homepage</h1>
       <p>
         lorem ipsu morem ipsum lorem ipsu morem ipsumlorem ipsu morem ipsumlorem
@@ -23,7 +20,6 @@ export default function Home() {
       <Link href="/portflio">
         <a>See My Portflio</a>
       </Link>
-      <Footer />
     </div>
   );
 }
